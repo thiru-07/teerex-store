7d7cef1787a225d4f32e1765398b4b010ca61cef
thiru-07 <mrthiru077@gmail.com>
2022-08-13 10:59:32 +0530

    Added pages and routing
    1. Installed react-icons for Icons and react-router-dom for routing
    2. Added Home, Cart pages and added routing to it

--- teerex-store/src/App.js
+++ teerex-store/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Header from './components/Header'
+import Home from './components/Home'
+import Cart from './components/Cart'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
-    </div>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/cart" exact element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- teerex-store/src/components/Header.js
+++ teerex-store/src/components/Header.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Navbar, Container, FormControl, Nav, Badge, Dropdown } from 'react-bootstrap'
+import { FaShoppingCart } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <Navbar bg='dark' variant='dark' style={{ height: 50 }}>
+            <Container>
+                <Navbar.Brand>
+                    <Link to={'/'}>Shopping Cart</Link>
+                </Navbar.Brand>
+                <Navbar.Text className='search'>
+                    <FormControl
+                        style={{ width: 500 }}
+                        placeholder='Search a product'
+                        className="m-auto" />
+                </Navbar.Text>
+                <Nav>
+                    <Dropdown>
+                        <Dropdown.Toggle variant='success'>
+                            <FaShoppingCart color='white' fontSize='25px' />
+                            <Badge>{10}</Badge>
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu style={{ minWidth: 370 }}>
+                            <span style={{ padding: 10 }}>Cart is empty!</span>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Nav>
+            </Container>
+        </Navbar>
     )
 }
 
